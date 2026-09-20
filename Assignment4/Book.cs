9309838816b6a1a3a186c5750a4232e4f91d73e8
original <original@DESKTOP-H4JMMMN>
2026-09-20 12:11:51 +0300

    answer 1

---
+++ Assignment4/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment4
+{
+    internal class Book
+    {
+        private string password = "secert";
+
+        
+    }
+}
--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Assignment4
+﻿using System.Net.Security;
+
+namespace Assignment4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // answer 1
+            #region answer 1
+            Book B = new Book();
+            //  B. password  مش هيظهر حاجه عشان انت مستخدم  accses  ال  privtr  ف هو مش هيهر غير ف   ال class بتاعه 
+
+           Console.WriteLine(Book.password);     // حلهم اني اعمل ال  accses modefier ل public
+            #endregion
+
+
         }
     }
 }
